649c1ad95396269f48a07f6463ceac237825af56
Keeners Navarro//KINGROCK2042 <130202458+KINGROCK2005@users.noreply.github.com>
2023-11-17 11:27:09 -0600

    Update profesor.java

--- Ejercicio3/src/profesor.java
+++ Ejercicio3/src/profesor.java
@@ -12,6 +12,33 @@ public class profesor extends estudiante{
         sexo = sexo;
         this.claseImpart = claseImpart;
     }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public int getclaseImpart() {
+        return claseImpart;
+    }
+
+    //Metodos setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    public void setclaseImpart(int claseImpart) {
+        this.claseImpart = claseImpart;
+    }  
+    
 
     public profesor(){
         
